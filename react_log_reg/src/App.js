d033800d128d2efb919521405b855fde31c057da
jerenteria <renteria.juan96@gmail.com>
2023-02-05 19:46:10 -0800

    almost done stuck trying to debug will need to come back

--- react_log_reg/src/App.js
+++ react_log_reg/src/App.js
@@ -1,41 +1,20 @@
-import React, { useState, useEffect} from 'react';
+import React, { useContext } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context'
+
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  
-  // runs after everything else in app has ran, then runs again if the dependencies change
-  useEffect(() => {
-    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
-
-    if(storedUserLoggedInInformation === '1') {
-      setIsLoggedIn(true);
-     }
-  }, []);
-
-
-  const loginHandler = (email, password) => {
-    
-    //isLoggedIn is a storage system in the browser independent of react go to application in inspect
-    localStorage.setItem('isLoggedIn', '1')
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    // when logging out clear isLoggedIn from localStorage in browser so user is not logged back in automatically when visitin website
-    localStorage.removeItem('isLoggedIn');
-    setIsLoggedIn(false);
-  };
+  const ctx = useContext(AuthContext);
 
   return (
     <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <MainHeader />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
       </main>
     </React.Fragment>
   );
---
+++ react_log_reg/src/store/auth-context.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+
+// creates context(app component wide state) object
+const AuthContext = React.createContext({
+    isLoggedIn: false,
+    onLogOut: () => {},
+    onLogIn: (email, password) => {}
+});
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    // runs after everything else in app has ran, then runs again if the dependencies change
+    useEffect(() => {
+        const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+        if (storedUserLoggedInInformation === '1') {
+            setIsLoggedIn(true);
+        }
+    }, []);
+
+
+    const logOutHandler = () => {
+        // when logging out clear isLoggedIn from localStorage in browser so user is not logged back in automatically when visitin website
+        localStorage.removeItem('isLoggedIn');
+        setIsLoggedIn(false);
+    };
+
+    const logInHandler = () => {
+        //isLoggedIn is a storage system in the browser independent of react go to application in inspect
+        localStorage.setItem('isLoggedIn', '1')
+        setIsLoggedIn(true);
+    };
+
+    return (
+        <AuthContext.Provider value={{
+            isLoggedIn: isLoggedIn,
+            onLogout: logOutHandler,
+            onLogIn: logInHandler
+        }}
+        >
+            {props.children}
+        </AuthContext.Provider>
+    )
+};
+
+export default AuthContext;

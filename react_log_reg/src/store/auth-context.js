a72d2e6f2100fa91c1f4894b86020daa86fb329b
jerenteria <renteria.juan96@gmail.com>
2023-02-10 21:54:08 -0800

    still cant find the bug

--- react_log_reg/src/store/auth-context.js
+++ react_log_reg/src/store/auth-context.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 
 // creates context(app component wide state) object
 const AuthContext = React.createContext({
